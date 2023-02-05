fd2eb63f7af53ae66296c4dce58154035d7f5302
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-05 17:05:23 +0300

    Translate template to arabic and English in common.lang.php files

---
+++ app/languages/ar/template/common.lang.php
@@ -0,0 +1,27 @@
+<?php
+
+// Header
+$content["text_header_title"]               = "لوحة التحكم";
+$content["text_header_personal_info"]       =  "المعلومات الشخصية";
+$content["text_header_setting"]             =  "الاعدادات ";
+$content["text_nav_logout"]                 =  "تسجيل الخروج";
+
+
+// Start Navigation
+$content["text_nav_search_session"]        = "ابحث عن خانة";
+$content["text_nav_general_setting"]        = "اعدادات عامة";
+$content["text_nav_users"]                  =  "المستخدمين";
+$content["text_nav_store"]                  =  "مخزن";
+$content["text_nav_product_category"]       =  "اصناف المنتجات";
+$content["text_nav_products"]               =  "الامنتجات";
+$content["text_nav_clients"]                =  "العملاء";
+$content["text_nav_suppliers"]              =  "الممولين";
+$content["text_nav_expenses"]               =  "نفقات";
+$content["text_nav_expenses_categories"]    =  "مصروفات الاصناف";
+$content["text_nav_daily_expenses"]         =  "المصروفات اليومية";
+$content["text_nav_transactions"]           =  "المعاملات";
+$content["text_nav_transactions_purchases"] =  "معاملات المشتريات";
+$content["text_nav_transactions_sales"]     =  "معاملات المبيعات";
+$content["text_nav_reports"]                =  "تقارير";
+$content["text_nav_Notifications"]          =  "الاشعارات";
+
---
+++ app/languages/en/template/common.lang.php
@@ -0,0 +1,27 @@
+<?php
+// Start Header
+$content["text_header_title"]                       = "Dashboard";
+$content["text_header_personal_info"]               =  "Personal Information";
+$content["text_header_setting"]                     =  "Setting";
+$content["text_nav_logout"]                         =  "Logout";
+
+// Start Navigation
+$content["text_nav_search_session"]             = "Search Session";
+$content["text_nav_general_setting"]            = "General Setting";
+$content["text_nav_users"]                      =  "users";
+$content["text_nav_store"]                      =  "Store";
+$content["text_nav_product_category"]           =  "Product Category";
+$content["text_nav_products"]                   =  "Products";
+$content["text_nav_clients"]                    =  "Clients";
+$content["text_nav_suppliers"]                  =  "Suppliers";
+$content["text_nav_expenses"]                   =  "Expenses";
+$content["text_nav_expenses_categories"]        =  "Expenses Categories";
+$content["text_nav_daily_expenses"]             =  "Daily Expenses";
+$content["text_nav_transactions"]               =  "Transactions";
+$content["text_nav_transactions_purchases"]     =  "Transactions Purchases";
+$content["text_nav_transactions_sales"]         =  "Transactions Sales";
+$content["text_nav_reports"]                    =  "Reports";
+$content["text_nav_Notifications"]              =  "Notifications";
+
+
+
