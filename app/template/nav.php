b0e3abbf3dca9c3c41af92b0b14dba47ead79749
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-05 17:02:37 +0300

    Add Links In Navigation Bar

--- app/template/nav.php
+++ app/template/nav.php
@@ -6,19 +6,85 @@
         <span class="name block mt-5 fs-20">Feras Barahmeh</span>
         <span class="privilege block mt-5 fs-15">Admin Application</span>
     </div>
-    <ul class="app_navigation mt-15 txt-l">
+
+    <div class="search-session mtb-15 w-fu relative">
+        <i class="fas fa-search search-icon"></i>
+        <input type="search" class="w-fu" id="search-session" placeholder="<?= $text_nav_search_session ?>" />
+    </div>
+
+    <ul class="app_navigation mt-15 txt-l" id="app_navigation">
         <li class="cursor-pointer">
             <i class="fa fa-cog" aria-hidden="true"></i>
             <a href="/"><span class="inline-block ml-5"><?= $text_nav_general_setting  ?></span></a>
         </li>
+
         <li class="cursor-pointer">
             <i class="fa fa-users" aria-hidden="true"></i>
-            <a href="/employee"><span class="inline-block ml-5"><?= $text_nav_users ?></span></a>
+            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_users ?></span></a>
         </li>
-<!--        <li class="cursor-pointer">-->
-<!--            <i class="fa fa-plus" aria-hidden="true"></i>-->
-<!--            <a href="/employee/add"><span class="inline-block ml-5">--><?php //= $text_nav_add_user ?><!--</span></a>-->
-<!--        </li>-->
+
+        <li class="cursor-pointer">
+            <i class="fa fa-store" aria-hidden="true"></i>
+            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_store ?></span></a>
+        </li>
+
+        <li class="cursor-pointer">
+            <i class="fa fa-tags" aria-hidden="true"></i>
+            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_product_category ?></span></a>
+        </li>
+
+        <li class="cursor-pointer">
+            <i class="fa fa-solid fa-users" aria-hidden="true"></i>
+            <a href="/clients"><span class="inline-block ml-5"><?= $text_nav_clients ?></span></a>
+        </li>
+
+        <li class="cursor-pointer">
+            <i class="fa fa-solid fa-parachute-box" aria-hidden="true"></i>
+            <a href="/suppliers"><span class="inline-block ml-5"><?= $text_nav_suppliers ?></span></a>
+        </li>
+
+        <li class="cursor-pointer">
+            <i class="fa fa-wallet" aria-hidden="true"></i>
+            <a href="/expenses"><span class="inline-block ml-5"><?= $text_nav_expenses ?></span></a>
+        </li>
+
+
+        <li class="cursor-pointer">
+            <i class="fa fa-receipt"  aria-hidden="true"></i>
+            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_expenses_categories ?></span></a>
+        </li>
+
+        <li class="cursor-pointer">
+            <i class="fa fa-credit-card" aria-hidden="true"></i>
+            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_daily_expenses ?></span></a>
+        </li>
+
+        <li class="cursor-pointer">
+            <i class="fas fa-exchange-alt"></i>
+            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_transactions ?></span></a>
+        </li>
+
+        <li class="cursor-pointer">
+            <i class="fa fa-money-check" aria-hidden="true"></i>
+            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_transactions_purchases ?></span></a>
+        </li>
+
+
+        <li class="cursor-pointer">
+            <i class="fas fa-handshake" aria-hidden="true"></i>
+            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_transactions_sales ?></span></a>
+        </li>
+
+        <li class="cursor-pointer">
+            <i class="fa fa-chart-pie" aria-hidden="true"></i>
+            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_reports ?></span></a>
+        </li>
+
+        <li class="cursor-pointer">
+            <i class="fa fa-bell" aria-hidden="true"></i>
+            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_Notifications ?></span></a>
+        </li>
+
 
     </ul>
 </nav>
