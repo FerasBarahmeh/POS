96dd049390926089e502ce2001e732d1a7a3e1f8
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-05 19:30:46 +0300

    Add DATA_TYPE_DATE in abstract model class

--- app/models/abstractmodel.php
+++ app/models/abstractmodel.php
@@ -11,6 +11,7 @@ class AbstractModel
     const DATA_TYPE_INT = \PDO::PARAM_INT;
     const DATA_TYPE_STR = \PDO::PARAM_STR;
     const DATA_TYPE_DECIMAL = 4;
+    const DATA_TYPE_DATE = 5;
     protected $_info = [];
 
     private function bindParams(\PDOStatement &$stmt): void
