0590a042264bc6939a839d01482cf29f395fc80c
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-06 13:24:04 +0300

    Refactor navigation bar (add sub menus)

--- app/template/nav.php
+++ app/template/nav.php
@@ -13,74 +13,83 @@
     </div>
 
     <ul class="app_navigation mt-15 txt-l" id="app_navigation">
-        <li class="cursor-pointer">
+        <li class="cursor-pointer main-li">
             <i class="fa fa-cog" aria-hidden="true"></i>
             <a href="/"><span class="inline-block ml-5"><?= $text_nav_general_setting  ?></span></a>
         </li>
 
-        <li class="cursor-pointer">
-            <i class="fa fa-users" aria-hidden="true"></i>
-            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_users ?></span></a>
+        <li class="cursor-pointer main-li sort-col grand-li">
+
+            <button class="between-ele w-fu">
+                <span class="inline-block ml-5"><i class="fas fa-exchange-alt"></i><?= $text_nav_transactions ?></span>
+                <i class="fa fa-angle-double-down angle "></i>
+            </button>
+
+            <ul class="sub-menu w-fu mtb-10 un-visible">
+                <li class="li-level-2 between-ele"><a href="/" class="sub-link"><?= $text_nav_transactions_purchases ?></a><i class="fa fa-shopping-cart" aria-hidden="true"></i></li>
+                <li class="li-level-2 between-ele"><a href="/" class="sub-link"><?= $text_nav_transactions_sales ?></a> <i class="fas fa-comment-dollar"></i></li>
+            </ul>
         </li>
 
-        <li class="cursor-pointer">
-            <i class="fa fa-store" aria-hidden="true"></i>
-            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_store ?></span></a>
-        </li>
-
-        <li class="cursor-pointer">
-            <i class="fa fa-tags" aria-hidden="true"></i>
-            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_product_category ?></span></a>
-        </li>
-
-        <li class="cursor-pointer">
-            <i class="fa fa-solid fa-users" aria-hidden="true"></i>
-            <a href="/clients"><span class="inline-block ml-5"><?= $text_nav_clients ?></span></a>
-        </li>
-
-        <li class="cursor-pointer">
-            <i class="fa fa-solid fa-parachute-box" aria-hidden="true"></i>
-            <a href="/suppliers"><span class="inline-block ml-5"><?= $text_nav_suppliers ?></span></a>
-        </li>
-
-        <li class="cursor-pointer">
-            <i class="fa fa-wallet" aria-hidden="true"></i>
-            <a href="/expenses"><span class="inline-block ml-5"><?= $text_nav_expenses ?></span></a>
-        </li>
-
-
-        <li class="cursor-pointer">
-            <i class="fa fa-receipt"  aria-hidden="true"></i>
-            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_expenses_categories ?></span></a>
-        </li>
-
-        <li class="cursor-pointer">
-            <i class="fa fa-credit-card" aria-hidden="true"></i>
-            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_daily_expenses ?></span></a>
-        </li>
-
-        <li class="cursor-pointer">
-            <i class="fas fa-exchange-alt"></i>
-            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_transactions ?></span></a>
-        </li>
-
-        <li class="cursor-pointer">
-            <i class="fa fa-money-check" aria-hidden="true"></i>
-            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_transactions_purchases ?></span></a>
-        </li>
-
-
-        <li class="cursor-pointer">
-            <i class="fas fa-handshake" aria-hidden="true"></i>
-            <a href="/users"><span class="inline-block ml-5"><?= $text_nav_transactions_sales ?></span></a>
-        </li>
-
-        <li class="cursor-pointer">
+        <li class="cursor-pointer main-li">
             <i class="fa fa-chart-pie" aria-hidden="true"></i>
             <a href="/users"><span class="inline-block ml-5"><?= $text_nav_reports ?></span></a>
         </li>
 
-        <li class="cursor-pointer">
+        <li class="cursor-pointer main-li sort-col grand-li">
+            <button class="between-ele w-fu">
+                <span class="inline-block ml-5"><i class="fa fa-store" aria-hidden="true"></i><?= $text_nav_store ?></span>
+                <i class="fa fa-angle-double-down angle "></i>
+            </button>
+
+            <ul class="sub-menu w-fu mtb-10 un-visible">
+                <li class="li-level-2 between-ele"><a href="/" class="sub-link"><?= $text_nav_product_category ?></a><i class="fa fa-shopping-cart" aria-hidden="true"></i></li>
+                <li class="li-level-2 between-ele"><a href="/" class="sub-link"><?= $text_nav_products ?></a> <i class="fas fa-comment-dollar"></i></li>
+            </ul>
+        </li>
+
+
+        <li class="cursor-pointer main-li sort-col grand-li">
+
+            <button class="between-ele w-fu">
+                <span class="inline-block ml-5"><i class="fa fa-wallet" aria-hidden="true"></i><?= $text_nav_expenses ?></span>
+                <i class="fa fa-angle-double-down angle "></i>
+            </button>
+
+            <ul class="sub-menu w-fu mtb-10 un-visible">
+                <li class="li-level-2 between-ele"><a href="/" class="sub-link"><?= $text_nav_expenses_categories ?></a><i class="fa fa-tags" aria-hidden="true"></i></li>
+                <li class="li-level-2 between-ele"><a href="/" class="sub-link"><?= $text_nav_daily_expenses ?></a> <i class="fa fa-credit-card" aria-hidden="true"></i></li>
+            </ul>
+        </li>
+
+
+        <li class="cursor-pointer main-li sort-col grand-li">
+
+            <button class="between-ele w-fu">
+                <span class="inline-block ml-5"><i class="fa fa-users" aria-hidden="true"></i><?= $text_nav_users ?></span>
+                <i class="fa fa-angle-double-down angle "></i>
+            </button>
+
+            <ul class="sub-menu w-fu mtb-10 un-visible">
+                <li class="li-level-2 between-ele"><a href="/" class="sub-link"><?= $text_nav_users_list ?></a><i class="fa fa-list" aria-hidden="true"></i></li>
+                <li class="li-level-2 between-ele"><a href="/" class="sub-link"><?= $text_nav_users_groups ?></a> <i class="fas fa-users" aria-hidden="true"></i></li>
+                <li class="li-level-2 between-ele"><a href="/" class="sub-link"><?= $text_nav_users_privileges ?></a> <i class="fas fa-user-secret" aria-hidden="true"></i></li>
+            </ul>
+        </li>
+
+
+        <li class="cursor-pointer main-li">
+            <i class="fa fa-solid fa-users" aria-hidden="true"></i>
+            <a href="/clients"><span class="inline-block ml-5"><?= $text_nav_clients ?></span></a>
+        </li>
+
+        <li class="cursor-pointer main-li">
+            <i class="fa fa-solid fa-parachute-box" aria-hidden="true"></i>
+            <a href="/suppliers"><span class="inline-block ml-5"><?= $text_nav_suppliers ?></span></a>
+        </li>
+
+
+        <li class="cursor-pointer main-li">
             <i class="fa fa-bell" aria-hidden="true"></i>
             <a href="/users"><span class="inline-block ml-5"><?= $text_nav_Notifications ?></span></a>
         </li>
