cfebd724934f4708e9f10832921788e54d46bdae
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-06 20:28:33 +0300

    call load language method in users controller

--- app/controllers/userscontroller.php
+++ app/controllers/userscontroller.php
@@ -8,6 +8,7 @@ class UsersController extends AbstractController
 {
     public function defaultAction()
     {
+        $this->_language->load("template.common");
         $this->_language->load("users.default");
         $this->_info["users"] = UserModel::getAll();
         $this->_renderView();
