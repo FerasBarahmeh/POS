6eb61f5905866aaf3fd2bae01bb43ed73acdf457
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-06 16:47:21 +0300

    create users groups languages words

---
+++ app/languages/ar/usersgroups/default.lang.php
@@ -0,0 +1,19 @@
+<?php
+
+// Title Page
+$content["title"] = "مجموعات المستخدمين";
+
+// Title Header
+$content["title_header"] = "مجموعات المستخدمين";
+
+//// content page
+//$content["text_content_add_user"]                = "اضافة مجموعة";
+//$content["text_content_search_user"]             = "ابحث مستخدم";
+//
+//// Table Users
+//$content["text_table_name_user"]                = "اسم المجموعة";
+//$content["text_table_password_user"]            = "الرقم السري";
+//$content["text_table_email_user"]               = "البريد الالكتروني";
+//$content["text_table_subscription_date_user"]   = "تاريخ التسجيل";
+//$content["text_table_last_login_user"]          = "اخر تسجيل دخول";
+//$content["text_table_phone_number_user"]        = "رقم الهاتف";
---
+++ app/languages/en/usersgroups/default.lang.php
@@ -0,0 +1,20 @@
+<?php
+
+// Title
+$content["title"] = "Users Groups";
+
+// Title Header
+$content["title_header"] = "User Groups";
+
+//// content page
+$content["text_content_add_group"]                = "Add Group";
+$content["text_content_search_group"]             = "Find Group";
+//
+//// Table Users
+$content["text_table_name_group"]                = "Name Group";
+$content["text_table_control"]                   = "control";
+//$content["text_table_password_user"]            = "Password";
+//$content["text_table_email_user"]               = "Email";
+//$content["text_table_subscription_date_user"]   = "Subscription Date";
+//$content["text_table_last_login_user"]          = "Last Login";
+//$content["text_table_phone_number_user"]        = "Phone Number";
