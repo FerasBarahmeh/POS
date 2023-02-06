a9120bed8a49d03e3c7330c949b45fea8da21cee
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-06 13:24:55 +0300

    edit search fetcher in main navigation bar

--- public/js/main.js
+++ public/js/main.js
@@ -20,32 +20,102 @@ dropDown.addEventListener("click", () => {
     menu.classList.toggle("visible");
 });
 
-// Hidden icon search when focus in input field search session
-const inputSearch = document.getElementById("search-session");
-
-if (inputSearch !== null) {
-    inputSearch.onfocus = () => {
-        inputSearch.parentElement.querySelector(".search-icon").classList.add("un-visible");
+// Start navigation Bar
+    function hiddenSearchIcon(inputSearch) {
+        inputSearch.onfocus = () => {
+            inputSearch.parentElement.querySelector(".search-icon").classList.add("un-visible");
+        }
+        inputSearch.onblur = () => {
+            inputSearch.parentElement.querySelector(".search-icon").classList.remove("un-visible");
+        }
     }
-    inputSearch.onblur = () => {
-        inputSearch.parentElement.querySelector(".search-icon").classList.remove("un-visible");
-    }
 
-    // Search In Sessions
-    const appNav = document.getElementById("app_navigation");
-    inputSearch.addEventListener("keyup", () => {
-       const content = inputSearch.value.toLowerCase();
-       const links = appNav.querySelectorAll("a");
-       links.forEach(link => {
-           const linkTo = link.innerText.toLowerCase();
-           const li = link.closest("li");
-           if (linkTo.search(content) === -1) {
-               li.classList.add("hidden");
+    function hiddenMainLi(li, contentInput) {
+
+        const links = li.querySelectorAll("a");
+        links.forEach(link => {
+           const content = link.textContent.toLowerCase();
+           if (content.search(contentInput) === -1) {
+               li.classList.add("un-visible");
            } else {
-               li.classList.remove("hidden");
+               li.classList.remove("un-visible");
            }
         });
+    }
+    function hiddenSubMenu(li, contentInput, subMenu) {
+        subMenu.classList.remove("un-visible")
+        const as = li.querySelectorAll("a");
+        const button = subMenu.closest("li").querySelector("button");
+        const span = button.querySelector("span");
+
+        as.forEach(link => {
+            const buttonContent = span.innerText.toLowerCase();
+            const content = link.textContent.toLowerCase() + buttonContent;
+
+            if (content.search(contentInput) === -1 ) {
+                li.classList.add("un-visible");
+                subMenu.closest(".main-li").classList.add("un-visible");
+            } else {
+                li.classList.remove("un-visible");
+                subMenu.closest(".main-li").classList.remove("un-visible");
+            }
+        });
+    }
+    function hiddenGrandLi(li, contentInput) {
+        const lis = li.querySelectorAll(".li-level-2");
+        let counter = 0;
+        const subMenu = li.querySelector(".sub-menu");
+        const numChild = subMenu.childElementCount;
+
+        lis.forEach(li => {
+            hiddenSubMenu(li, contentInput, subMenu);
+            if (li.classList.contains("un-visible")) {
+                counter++;
+            }
+
+            if (counter === numChild) {
+                subMenu.classList.add("un-visible");
+            } else {
+                subMenu.classList.remove("un-visible");
+            }
+
+        });
+    }
+
+    // Hidden icon search when focus in input field search session
+    const inputSearch = document.getElementById("search-session");
+
+    if (inputSearch !== null) {
+
+        hiddenSearchIcon(inputSearch);
+
+        // Search In Sessions
+        const appNav    = document.getElementById("app_navigation");
+        const lis  = appNav.querySelectorAll("li.main-li");
+
+        inputSearch.addEventListener("keyup", () => {
+           const contentInput = inputSearch.value.toLowerCase();
+
+           lis.forEach(li => {
+                if (! li.classList.contains("grand-li")) {
+                    hiddenMainLi(li, contentInput);
+                } else {
+                    hiddenGrandLi(li, contentInput);
+                }
+           });
+        });
+    }
+
+    // Hidden SubMenu
+    const grandLis = document.querySelectorAll(".grand-li");
+
+    grandLis.forEach(grandLi =>  {
+       grandLi.addEventListener('click', () => {
+          grandLi.querySelector(".sub-menu").classList.toggle('un-visible');
+          grandLi.querySelector(".angle").classList.toggle("angle-rotate");
+       });
     });
-}
+
+
 
 
