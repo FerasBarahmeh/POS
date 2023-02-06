adf0de33fb69d6f96fa3f2da0a2ccf783d27dcbf
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-06 20:23:42 +0300

    change table name in user group model

--- app/models/usergroupmodel.php
+++ app/models/usergroupmodel.php
@@ -7,7 +7,7 @@ class UserGroupModel extends AbstractModel
     public $groupId;
     public $groupName;
 
-    protected static $tableName = "users";
+    protected static $tableName = "users_groups";
 
     protected static array $tableSchema = [
         "GroupName"              => self::DATA_TYPE_STR,
