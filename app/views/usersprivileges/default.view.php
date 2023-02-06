0cd2acd2beeb440b5027cfe1ce0f2799881b42ed
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-06 20:30:49 +0300

    edit view user privileges

---
+++ app/views/usersprivileges/default.view.php
@@ -0,0 +1,43 @@
+<h1 class="title-header"><?= $title_header ?></h1>
+
+<div class="header-option flex mt-10">
+    <a href="/usersprivileges/add" class="add-user"><?= $text_content_add_privilege ?> <i class="fa fa-plus ml-10"></i></a>
+    <div class="search">
+        <label for="find-user"></label><input type="search" id="find-user"  placeholder="<?= $text_content_search_privilege ?>">
+        <i class="fa fa-search lens"></i>
+    </div>
+</div>
+
+<!-- Show Employees -->
+<div class="users responsive-table mt-20" id="employees-table">
+    <table class="">
+        <thead>
+        <tr>
+            <th><?= $text_table_name_privilege ?></th>
+            <th><?= $text_table_control ?></th>
+        </tr>
+        </thead>
+
+        <tbody>
+
+                <?php
+                    if (! empty($privileges) && isset($privileges)) {
+                        foreach ($privileges as $privilege) {
+                            ?>
+                            <tr class="row-each-employee">
+                                <td class="name-user-row"><?= $privilege->Privilege ?></td>
+                                <td class="controller-btns">
+                                    <a href="employee/edit/<?= $privilege->PrivilegeId ?>"><i class="fas fa-edit"></i></a>
+                                    <a href="employee/delete/<?= $privilege->PrivilegeId ?>"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                                    <a href=""><i class="fa fa-bell"  aria-hidden="true"></i></a>
+                                </td>
+
+                            </tr>
+                            <?php
+                        }
+                    }
+
+                ?>
+        </tbody>
+    </table>
+</div>
