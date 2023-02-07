28487946dfd7ac0a815c67a2cd11d1a568c4d40a
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-07 17:57:02 +0300

    split input attribute and add placeholder space to apply fixed label idea

--- app/views/usersprivileges/add.view.php
+++ app/views/usersprivileges/add.view.php
@@ -3,12 +3,18 @@
 <form class="add-form" method="POST" enctype="application/x-www-form-urlencoded">
     <fieldset class="row pb-10">
         <div class="field">
-            <input type="text" name="privilege_title" id="PrivilegeTitle" maxlength="30" required />
+            <!-- Must Set space in placeholder to apply label animation -->
+            <input type="text"
+                   name="privilege_title" id="PrivilegeTitle"
+                   placeholder=" " minlength="3"
+                   maxlength="30" required autocomplete="off"/>
             <label for="privilege_title"> <?= $text_title_privilege; ?> </label>
         </div>
 
         <div class="field">
-            <input type="text" name="privilege"  id="PrivilegeTitle" maxlength="30" required/>
+            <!-- Must Set space in placeholder to apply label animation -->
+            <input type="text" name="privilege"  id="PrivilegeTitle" placeholder=" "
+                   minlength="3" maxlength="30" required autocomplete="off"/>
             <label for=""> <?= $text_privilege ?> </label>
         </div>
 
