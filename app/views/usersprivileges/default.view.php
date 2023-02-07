b0437a5cd33488ddc395339864f5c3a2839834ec
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-07 17:59:38 +0300

    Add links in control filed

--- app/views/usersprivileges/default.view.php
+++ app/views/usersprivileges/default.view.php
@@ -14,6 +14,7 @@
         <thead>
         <tr>
             <th><?= $text_table_name_privilege ?></th>
+            <th><?= $text_table_privilege ?></th>
             <th><?= $text_table_control ?></th>
         </tr>
         </thead>
@@ -25,11 +26,16 @@
                         foreach ($privileges as $privilege) {
                             ?>
                             <tr class="row-each-employee">
+                                <td class="name-user-row"><?= $privilege->PrivilegeTitle ?></td>
                                 <td class="name-user-row"><?= $privilege->Privilege ?></td>
                                 <td class="controller-btns">
-                                    <a href="employee/edit/<?= $privilege->PrivilegeId ?>"><i class="fas fa-edit"></i></a>
-                                    <a href="employee/delete/<?= $privilege->PrivilegeId ?>"><i class="fa fa-trash" aria-hidden="true"></i></a>
-                                    <a href=""><i class="fa fa-bell"  aria-hidden="true"></i></a>
+                                    <a href="/usersprivileges/edit/<?= $privilege->PrivilegeId ?>">
+                                        <i class="fa fa-edit" aria-hidden="true"></i>
+                                    </a>
+                                    <a href="/usersprivileges/delete/<?= $privilege->PrivilegeId ?>">
+                                        <i class="fa fa-trash" aria-hidden="true"></i>
+                                    </a>
+                                    <a href="/"><i class="fa fa-bell"  aria-hidden="true"></i></a>
                                 </td>
 
                             </tr>
