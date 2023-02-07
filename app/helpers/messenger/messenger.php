412f630bb3e31d1f2d0b6042cc699575a8201817
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-07 22:57:02 +0300

    create messenger class and popup method

---
+++ app/helpers/messenger/messenger.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace APP\Helpers\Messenger;
+
+class Messenger
+{
+    /**
+     * This method to set Popup box in page depends on shortcut framework
+     *
+     * Description.
+     *
+     * @param string $contentMassage tack massage you will set in popup box
+     * @param string $expressSymbol Set Symbol express to massage popup box
+     * @param string $typeStyle This variable to control Theme
+     * add class from shortcut framework if The action danger (Imported action add danger class)
+     * if Action come after action Success add class success or done
+     * @param string|null $typeAction This variable to determent type action after click ok button in popup
+     * if link this is mean will click to link to do action
+     * @param string|null $id The element to apply action on him (Must Be Hidden) and add 'pop-on-click' class to another
+     * element Must Set After type action direct
+     * @return void This method To Set Popup Box Structure.
+     * @since 1.0.0
+     * @access Public
+     *
+     */
+    public static function popup(string $contentMassage="Are You Sure" ,  string  $expressSymbol = '!', string $typeStyle="danger", string $typeAction=null, string $id=null): void
+    {
+        ?>
+        <div class="card <?= $typeStyle ?> <?= $typeAction ?> <?= $id ?>">
+            <div class="symbol flex ">
+                <span class="express-symbol  <?= $typeStyle ?>"><?= $expressSymbol ?></span>
+                <span class="delete" id="reject-popup">X</span>
+            </div>
+            <p class="content <?= $typeStyle ?>"><?= $contentMassage ?></p>
+            <span class="ok" id="accepted-popup">OK</span>
+        </div>
+        <?php
+    }
+}
