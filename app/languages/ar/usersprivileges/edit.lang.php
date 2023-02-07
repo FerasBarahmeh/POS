49cca5b7c7b133b7c162bdc44cce76b1a1abff69
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-07 17:58:52 +0300

    add words in files lang to privilege pages

---
+++ app/languages/ar/usersprivileges/edit.lang.php
@@ -0,0 +1,11 @@
+<?php
+
+// Title
+$content["title"] = "تعديل الصلاحيات";
+
+// Title Header
+$content["title_header"] = "تعديل الصلاحيات";
+
+// Inputs
+$content["text_title_privilege"]                 = "Name Privileges";
+$content["text_privilege"]                      = "Privilege";
---
+++ app/languages/en/usersprivileges/edit.lang.php
@@ -0,0 +1,11 @@
+<?php
+
+// Title
+$content["title"] = "Edit Privilege";
+
+// Title Header
+$content["title_header"] = "Edit Privilege";
+
+// Inputs
+$content["text_title_privilege"]                 = "Name Privileges";
+$content["text_privilege"]                      = "Privilege";
