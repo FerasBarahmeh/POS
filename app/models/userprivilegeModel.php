951d5c3ef12e62c4c493e55da90d924a65a7f653
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-07 17:53:13 +0300

    rename property ad a column name in db table

--- app/models/userprivilegeModel.php
+++ app/models/userprivilegeModel.php
@@ -4,13 +4,16 @@ namespace APP\Models;
 
 class UserPrivilegeModel extends AbstractModel
 {
-    public $privilegeId;
-    public $privilege;
+    public  $PrivilegeId;
+    public  $Privilege;
+    public  $PrivilegeTitle;
+
 
     protected static $tableName = "users_privileges";
 
     protected static array $tableSchema = [
         "Privilege"              => self::DATA_TYPE_STR,
+        "PrivilegeTitle"         => self::DATA_TYPE_STR,
     ];
 
     protected static string $primaryKey = "PrivilegeId";
