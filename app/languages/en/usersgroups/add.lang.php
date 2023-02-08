6cd10083a610927f116aaed91efef096b364346c
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-08 22:16:56 +0300

    add words in files language

---
+++ app/languages/en/usersgroups/add.lang.php
@@ -0,0 +1,13 @@
+<?php
+
+// Title
+$content["title"] = "Add Group";
+
+// Title Header
+$content["title_header"] = "Add Group";
+
+// Inputs
+$content["text_name_group"]               = "Name Group";
+$content["text_privilege"]                      = "Privilege";
+$content["text_choose_privileges_group"]                      = "Choose Privileges To This Group";
+
---
+++ app/languages/en/usersgroups/edit.lang.php
@@ -0,0 +1,13 @@
+<?php
+
+// Title
+$content["title"] = "Edit Group";
+
+// Title Header
+$content["title_header"] = "Edit Group";
+
+// Inputs
+$content["text_name_group"]               = "Name Group";
+$content["text_privilege"]                      = "Privilege";
+$content["text_choose_privileges_group"]                      = "Choose Privileges To This Group";
+
