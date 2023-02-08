52ec7d06e61731883f28ec0c18b8b24f08666f62
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-08 22:17:45 +0300

    create edit group view

---
+++ app/views/usersgroups/edit.view.php
@@ -0,0 +1,38 @@
+<h1 class="title-header"><?= $title_header ?></h1>
+
+<form class="add-form" method="POST" enctype="application/x-www-form-urlencoded">
+    <fieldset class="row pb-10">
+        <div class="field ">
+            <!-- Must Set space in placeholder to apply label animation -->
+            <input type="text" value="<?= $group->GroupName ?>"
+                   name="GroupName" id="PrivilegeTitle"
+                   placeholder=" " required autocomplete="off"/>
+            <label for="GroupName"> <?= $text_name_group; ?> </label>
+        </div>
+    </fieldset>
+
+    <!-- Check Box Privileges -->
+    <fieldset class="privileges-to-groups flex sort-col mtb-20 plr-15">
+        <label class="label-privilege-add-group mb-15"><?= $text_choose_privileges_group ?></label>
+
+        <?php
+            foreach ($privileges as $privilege) {
+
+                ?>
+
+                    <div class="field">
+                        <input type="checkbox" id="privilege-<?= $privilege->privilegeId ?>" name="privileges[]"
+                            <?= in_array($privilege->PrivilegeId, $groupPrivilege) ? "checked" : '' ?>
+                               value="<?= $privilege->PrivilegeId ?>" class="cursor-pointer">
+                        <label for="privilege-<?= $privilege->PrivilegeId ?>"><?= $privilege->PrivilegeTitle ?></label>
+                    </div>
+                <?php
+            }
+        ?>
+
+    </fieldset>
+
+    <fieldset class="row submit-btn-container">
+        <input type="submit" class="submit-btn" value="Edit" name="edit">
+    </fieldset>
+</form>
