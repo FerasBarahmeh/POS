3fd4ad7ae74d58249540f922a9787d979c651390
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-09 15:41:13 +0300

    Create Registration class

---
+++ app/LIB/Registraion.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace APP\LIP;
+
+class Registration
+{
+    private static $_instance;
+
+    private function __construct(){}
+
+    private function __clone(){}
+
+    public function getInstance(): Registration
+    {
+        if (self::$_instance === null) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    public function __set(string $name, $value): void
+    {
+        $this->$name = $value;
+    }
+    public function __get(string $name)
+    {
+        return $this->$name;
+    }
+}
