67cb085de9b1689072784dfe66d2479bea3506fb
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-10 22:57:51 +0300

    add words languages in files add users .lang

---
+++ app/languages/ar/users/add.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+// Title
+$content["title"] = "اضافة مستخدم";
+
+// Title Header
+$content["title_header"] = "اضافة مستخدم";
+
+
+// Field
+$content["table_user_name"]             = "اسم المستخدم";
+$content["table_password"]              = "كلمة المرور";
+$content["table_confirm_password"]      = "تأكيد كلمة المرور";
+$content["table_email"]                 = "البريد الالكتروني";
+$content["table_subscription_date"]     = "تاريخ الاضافة";
+$content["table_phone_number"]          = "رقم الهاتف";
+$content["table_group_id"]              = "المجموعة التي ينتمي لها المستخدم";
---
+++ app/languages/en/users/add.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+// Title
+$content["title"] = "Add User";
+
+// Title Header
+$content["title_header"] = "Add User";
+
+
+// Field
+$content["table_user_name"]             = "User Name";
+$content["table_password"]              = "Password";
+$content["table_confirm_password"]      = "Confirm Password";
+$content["table_email"]                 = "Email";
+$content["table_subscription_date"]     = "Subscription Date";
+$content["table_phone_number"]          = "Phone Number";
+$content["table_group_id"]              = "Group Id";
