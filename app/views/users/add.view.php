52b52db2dc0d81166cc85676a5f78ab7e271d48e
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-12 23:24:51 +0300

    apply get status post in add users view

--- app/views/users/add.view.php
+++ app/views/users/add.view.php
@@ -4,24 +4,30 @@
     <fieldset class="row">
         <div class="field">
             <!-- Must Set space in placeholder to apply label animation  -->
-            <input type="text"
-                   name="UserName" id="UserName"
+            <input type="text" name="UserName" id="UserName"
+                   value="<?= $this->getStorePost("UserName") ?>"
                    minlength="4" maxlength="12" required
-                   placeholder=" "   autocomplete="off"/>
+                   placeholder=" "   autocomplete="off"  />
+
             <label for="UserName"> <?= $table_UserName ?></label>
         </div>
 
         <div class="field">
             <!-- Must Set space in placeholder to apply label animation -->
-            <input type="password" name="Password"  id="Password" placeholder=" "
-                   minlength="7" maxlength="60" required autocomplete="off"/>
+            <input type="password" name="Password"  id="Password"
+                    value="<?= $this->getStorePost("Password") ?>"
+                   placeholder=" " minlength="7" maxlength="60"
+                   required autocomplete="off"/>
             <label for="Password"> <?= $table_Password ?> </label>
         </div>
 
         <div class="field">
             <!-- Must Set space in placeholder to apply label animation -->
-            <input type="password" name="confirm_password"  id="confirm_password" placeholder=" "
-                   minlength="7" maxlength="60" required autocomplete="off"/>
+            <input type="password" name="confirm_password"  id="confirm_password"
+                    value="<?= $this->getStorePost("confirm_password") ?>"
+                   placeholder=" " minlength="7" maxlength="60"
+                   required autocomplete="off"/>
+
             <label for="confirm_password"> <?=  $table_confirm_password ?> </label>
         </div>
 
@@ -31,19 +37,21 @@
     <fieldset class="row">
         <div class="field">
             <!-- Must Set space in placeholder to apply label animation -->
-            <input type="email"
-                   name="Email" id="Email"
-                   placeholder=" " minlength="10"
-                   maxlength="30" required autocomplete="off"/>
+            <input type="email"  name="Email" id="Email"
+                    value="<?= $this->getStorePost("Email") ?>"
+                   placeholder=" " minlength="10" maxlength="30"
+                   required autocomplete="off"/>
+
             <label for="Email"> <?= $table_Email ?></label>
         </div>
 
         <div class="field">
             <!-- Must Set space in placeholder to apply label animation -->
-            <input type="email"
-                   name="confirm_email" id="confirm_email"
-                   placeholder=" " minlength="10"
-                   maxlength="30" required autocomplete="off"/>
+            <input type="email" name="confirm_email" id="confirm_email"
+                    value="<?= $this->getStorePost("confirm_email") ?>"
+                   placeholder=" " minlength="10" maxlength="30"
+                   required autocomplete="off"/>
+
             <label for="confirm_email"> <?= $table_confirm_email ?></label>
         </div>
 
@@ -53,7 +61,7 @@
         <div class="field">
             <!-- Must Set space in placeholder to apply label animation -->
             <select name="GroupId" id="GroupId" required autocomplete="off">
-                <option class="selected"><?= $table_GroupId ?></option>
+                <option class="selected" value="<?= $this->getStorePost("GroupId") ?>"><?= $table_GroupId ?></option>
                 <?php
                     if ($groups) {
                         foreach ($groups as $group) {
@@ -70,8 +78,10 @@
 
         <div class="field">
             <!-- Must Set space in placeholder to apply label animation -->
-            <input type="text" name="PhoneNumber"  id="PhoneNumber" placeholder=" "
-                   minlength="3" maxlength="15" required autocomplete="off"/>
+            <input type="text" name="PhoneNumber"  id="PhoneNumber"
+                   value="<?= $this->getStorePost("PhoneNumber") ?>"
+                   placeholder=" " minlength="3" maxlength="15"
+                   required autocomplete="off"/>
             <label for="PhoneNumber"> <?= $table_PhoneNumber ?> </label>
         </div>
 
