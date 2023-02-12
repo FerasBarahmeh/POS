681e39fb4c608ee6e11cdfd6df27cf4168aaba18
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-13 00:10:24 +0300

    create add user method

--- app/controllers/userscontroller.php
+++ app/controllers/userscontroller.php
@@ -2,7 +2,9 @@
 
 namespace APP\Controllers;
 
+use APP\Helpers\PublicHelper\PublicHelper;
 use APP\LIB\FilterInput;
+use APP\LIB\Messenger;
 use APP\Models\UserGroupModel;
 use APP\Models\UserModel;
 use function APP\pr;
@@ -10,6 +12,7 @@ use function APP\pr;
 class UsersController extends AbstractController
 {
     use FilterInput;
+    use PublicHelper;
     private array $_rolesValid = [
         "UserName"          => ["req", "alphaNum",  "between(4,12)",],
         "Password"          => ["req", "between(7,60)", "alphaNum", "compare(confirm_password)"],
@@ -28,17 +31,37 @@ class UsersController extends AbstractController
     }
     public function addAction()
     {
+        // TODO: Check if user exist before insert in db ajax
         $this->language->load("template.common");
         $this->language->load("users.add");
         $this->language->load("messages.errors");
+        $this->language->load("users.messages");
 
         $this->_info["groups"] = UserGroupModel::getAll();
 
         if (isset($_POST["add"]) && $this->isAppropriate($this->_rolesValid, $_POST) )  {
             $user = new UserModel();
             $user->UserName = $this->filterStr($_POST["UserName"]);
+            $user->encryptionPassword($_POST["Password"]);
+            $user->Email                = $this->filterStr($_POST["Email"]);
+            $user->SubscriptionDate     = date("Y-m-d H:i:s");
+            $user->LastLogin            = date("Y-m-d H:i:s");
+            $user->GroupId              = $this->filterInt($_POST["GroupId"]);
+            $user->PhoneNumber          = $this->filterStr($_POST["PhoneNumber"]);
+            $user->Status               = 1;
 
+            if ($user->save()) {
+                $this->message->addMessage(
+                    $this->language->get("message_add_success"),
+                );
+            } else {
+                $this->message->addMessage(
+                    $this->language->get("message_add_fail"),
+                    Messenger::MESSAGE_DANGER
+                );
+            }
 
+            $this->redirect("/users");
 
         }
         $this->_renderView();
