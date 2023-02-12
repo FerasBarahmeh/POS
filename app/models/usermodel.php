395a61d818558b1138fa0e8ff086742922d52f33
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-12 23:13:41 +0300

    change data type SubscriptionDate,  create encryptionPassword method to encryption password, override getAll method add to query inner join statement

--- app/models/usermodel.php
+++ app/models/usermodel.php
@@ -20,7 +20,7 @@ class UserModel extends AbstractModel
         "UserName"              => self::DATA_TYPE_STR,
         "Password"              => self::DATA_TYPE_STR,
         "Email"                 => self::DATA_TYPE_STR,
-        "SubscriptionDate"      => self::DATA_TYPE_DATE,
+        "SubscriptionDate"      => self::DATA_TYPE_STR,
         "LastLogin"             => self::DATA_TYPE_STR,
         "GroupId"               => self::DATA_TYPE_INT,
         "Status"                => self::DATA_TYPE_INT,
@@ -30,4 +30,17 @@ class UserModel extends AbstractModel
 
     protected static string $primaryKey = "UserId";
 
+    public function encryptionPassword($pass)
+    {
+        $salt = '$2a$07$bnzferasbarahmeh$';
+        $this->Password = crypt($pass, $salt);
+    }
+
+    public static function getAll(): bool|\ArrayIterator
+    {
+        return (new UserModel)->get(
+            "SELECT u.*, ug.GroupName FROM " . self::$tableName .
+            " as u INNER JOIN users_groups as ug ON ug.GroupId = u.GroupId "
+        );
+    }
 }
