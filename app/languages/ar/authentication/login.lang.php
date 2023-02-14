d5be88c38ef91325892285119a4138da7f93a2a3
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-14 23:00:06 +0300

    add some words in languages files

---
+++ app/languages/ar/authentication/login.lang.php
@@ -0,0 +1,14 @@
+<?php
+// Title
+$content["title"] = "تسجيل الدخول";
+
+// form
+$content["welcome_message"]         = "مرحبا بعودتك";
+$content["placeholder_username"]    = "اسم المستخدم";
+$content["placeholder_password"]    = "كلمة المرور";
+$content["value_btn_login"]         = "تسجيل الدخول";
+
+// messages
+$content["message_user_not_found"]          = "اسم المستخدم غير موجود او كلمة المرور خاطئة";
+$content["message_user_disabled"]           = "هاذا المستخدم محضور يرجى مراجعة الادارة";
+$content["message_user_exist"]              = "مرحبا لعودتك";
---
+++ app/languages/en/authentication/login.lang.php
@@ -0,0 +1,14 @@
+<?php
+// Title
+$content["title"] = "Login";
+
+// form
+$content["welcome_message"]         = "Welcome Back !!";
+$content["placeholder_username"]    = "Username";
+$content["placeholder_password"]    = "Password";
+$content["value_btn_login"]         = "Login";
+
+// messages
+$content["message_user_not_registration"]   = "Username not exist or Password not correct";
+$content["message_user_disabled"]           = "This user disabled";
+$content["message_user_exist"]              = "Welcome Back";
