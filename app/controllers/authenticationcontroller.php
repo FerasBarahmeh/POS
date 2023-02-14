99d0d1b67adfbc9686110c3def84343dfccec70d
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-14 22:57:48 +0300

    create authentication controller class

---
+++ app/controllers/authenticationcontroller.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace APP\Controllers;
+
+use APP\Helpers\PublicHelper\PublicHelper;
+use APP\LIB\Messenger;
+use APP\Models\UserModel;
+use UserStatus;
+use function APP\pr;
+
+class AuthenticationController extends AbstractController
+{
+    use PublicHelper;
+    use UserStatus;
+    private function setMessageLogin($result)
+    {
+        if ($result == self::$UserNotRegistration) {
+            $this->message->addMessage($this->language->get("message_user_not_registration"), Messenger::MESSAGE_DANGER);
+        } elseif ($result == self::$UserDisable) {
+            $this->message->addMessage($this->language->get("message_user_disabled"), Messenger::MESSAGE_DANGER);
+        }
+        if($result == self::$UserValid) {
+            $this->message->addMessage($this->language->get("welcome_message"));
+            $this->redirect("/");
+        }
+
+    }
+    public function loginAction()
+    {
+        $this->language->load("authentication.login");
+        $this->_template->choosePartsUI([
+            NAME_VIEW_TEMPLATE_KEY                  => ":action_view",
+        ]);
+
+        if (isset($_POST["login"])) {
+            $result = UserModel::authentication($_POST["UserName"], $_POST["Password"], $this->session);
+            $this->setMessageLogin($result);
+        }
+
+        $this->_renderView();
+    }
+}
