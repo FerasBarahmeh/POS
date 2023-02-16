225079066094350fec0db9abee0b0b0b2b367dc9
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-16 17:45:30 +0300

    create authorizedAccess method

--- app/LIB/authentication.php
+++ app/LIB/authentication.php
@@ -7,6 +7,13 @@ use function APP\pr;
 class Authentication
 {
     private static $_instance;
+    private array $_authorizedURLs = [
+        "/index/default",
+        "/language/default",
+        "/authentication/logout",
+        "/denyauthorizeaccess/default",
+        "/notfound/notfound",
+    ];
     private $_session;
     private function __construct($_session)
     {
@@ -25,5 +32,14 @@ class Authentication
     public function isAuthenticated(): bool
     {
         return isset($this->_session->user);
+    }
+
+    public function authorizedAccess($controller, $action): bool
+    {
+        $url = '/' . $controller . '/' . $action;
+        if (in_array($url, $this->_authorizedURLs) || in_array($url, $this->_session->user->privileges)) {
+            return true;
+        }
+        return false;
     }
 }
