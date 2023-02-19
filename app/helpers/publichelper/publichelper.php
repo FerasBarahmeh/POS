c51f006956beaec83749c659c0a0669b9679433c
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-19 22:58:10 +0300

    create getClassProperties and getClassValuesProperties method in PublicHelper trait

--- app/helpers/publichelper/publichelper.php
+++ app/helpers/publichelper/publichelper.php
@@ -4,6 +4,9 @@ namespace APP\Helpers\PublicHelper;
 
 
 
+use ReflectionClass;
+use function APP\pr;
+
 trait PublicHelper
 {
     public function redirect($path): void
@@ -13,4 +16,29 @@ trait PublicHelper
         exit;
     }
 
+    /**
+     * @throws \ReflectionException
+     */
+    public function getClassProperties($obj, $getClassName=true): array
+    {
+        $data = (new ReflectionClass($obj::class))->getProperties();
+        if (! $getClassName) {
+            return $data;
+        }
+
+        $nameProperties = [];
+        foreach ($data as $row) {
+            $nameProperties[] = $row->name;
+        }
+
+        return $nameProperties;
+    }
+
+    /**
+     * @throws \ReflectionException
+     */
+    public function getClassValuesProperties($obj, $getClassName=true): array
+    {
+        return (new ReflectionClass($obj::class))->getDefaultProperties();
+    }
 }
