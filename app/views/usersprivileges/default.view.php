33ffe5e4e49d134d0cbdc8397f2369a81467fb75
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-22 23:47:53 +0300

    Create pagination table tools in shortcut framework

--- app/views/usersprivileges/default.view.php
+++ app/views/usersprivileges/default.view.php
@@ -1,51 +1,50 @@
 <h1 class="title-header"><?= $title_header ?></h1>
 
 <div class="header-option flex mt-10">
-    <a href="/usersprivileges/add" class="add-user"><?= $text_content_add_privilege ?> <i class="fa fa-plus ml-10"></i></a>
+    <a href="/usersprivileges/add" class=""><?= $text_content_add_privilege ?> <i class="fa fa-plus ml-10"></i></a>
     <div class="search">
-        <label for="find-user"></label><input type="search" id="find-user"  placeholder="<?= $text_content_search_privilege ?>">
+        <label for="find-user"></label><input type="search"   placeholder="<?= $text_content_search_privilege ?>">
         <i class="fa fa-search lens"></i>
     </div>
 </div>
 
-    <!-- Show Privileges -->
-    <div class="container-table responsive-table mt-20" id="employees-table">
-        <table class="">
-            <thead>
-                <tr>
-                    <th><?= $text_table_name_privilege ?></th>
-                    <th><?= $text_table_privilege ?></th>
-                    <th><?= $text_table_control ?></th>
-                </tr>
-            </thead>
+<!-- Show Privileges -->
+<div class="container-table responsive-table mt-20" id="employees-table">
+    <table class="pagination-table">
+        <thead>
+        <tr>
+            <th><?= $text_table_name_privilege ?></th>
+            <th><?= $text_table_privilege ?></th>
+            <th><?= $text_table_control ?></th>
+        </tr>
+        </thead>
 
-            <tbody>
+        <tbody>
+            <?php
+                if (! empty($privileges)) {
+                    foreach ($privileges as $privilege) {
+                        ?>
+                            <tr class="">
+                                <td class=""><?= $privilege->PrivilegeTitle ?></td>
+                                <td class=""><?= $privilege->Privilege ?></td>
+                                <td class="controller-btns">
+                                    <a href="/usersprivileges/edit/<?= $privilege->PrivilegeId ?>">
+                                        <i class="fa fa-edit" aria-hidden="true"></i>
+                                    </a>
+                                    <a class="hidden"  href="/usersprivileges/delete/<?= $privilege->PrivilegeId ?>" id="delete"></a>
+                                    <?php $this->popup(
+                                        "you wont delete privilege",
+                                        '!', $typeStyle="danger",
+                                        $typeAction="link",
+                                        $id="delete-privilege"); ?>
+                                    <span class="pop-on-click danger-style cursor-pointer"><i class="fa fa-trash" aria-hidden="true"></i></span>
 
-                <?php
-                    if (! empty($privileges)) {
-                        foreach ($privileges as $privilege) {
-                            ?>
-                                <tr class="row-each-employee">
-                                    <td class="name-user-row"><?= $privilege->PrivilegeTitle ?></td>
-                                    <td class="name-user-row"><?= $privilege->Privilege ?></td>
-                                    <td class="controller-btns">
-                                        <a href="/usersprivileges/edit/<?= $privilege->PrivilegeId ?>">
-                                            <i class="fa fa-edit" aria-hidden="true"></i>
-                                        </a>
-                                        <a class="hidden"  href="/usersprivileges/delete/<?= $privilege->PrivilegeId ?>" id="delete"></a>
-                                        <?php $this->popup(
-                                                "you wont delete privilege",
-                                                '!', $typeStyle="danger",
-                                                $typeAction="link",
-                                                $id="delete-privilege"); ?>
-                                        <span class="pop-on-click danger-style cursor-pointer"><i class="fa fa-trash" aria-hidden="true"></i></span>
-
-                                    </td>
-                                </tr>
-                            <?php
-                        }
+                                </td>
+                            </tr>
+                        <?php
                     }
-                ?>
-            </tbody>
-        </table>
-    </div>
+                }
+            ?>
+        </tbody>
+    </table>
+</div>
--- public/js/shortcut.js
+++ public/js/shortcut.js
@@ -1,3 +1,8 @@
+// global funstions
+function insertAfter(afterTo, newNode) {
+    afterTo.parentNode.insertBefore(newNode, afterTo.nextSibling);
+}
+
 // TODO: Convert Structure Delete To AJAX
 const cardPopup = document.querySelector(".card");
 const classCard = [...cardPopup.classList];
@@ -62,3 +67,296 @@ if (acceptedPopups != null) {
         });
     })
 }
+
+function getPreviousSlide(rows, bodyTable, rowsBerSlid, pageNumber) {
+    bodyTable.innerHTML = '';
+    pageNumber --;
+
+    console.log("page Number = " + pageNumber);
+    console.log("rowsBerSlid = " + rowsBerSlid)
+
+    let start = (pageNumber * rowsBerSlid) - rowsBerSlid ;
+    let end  = start + rowsBerSlid  ;
+
+    console.log("start = " + start);
+    console.log("end = " + end)
+
+    let rowsSlide = Array.from(rows).slice(start, end);
+
+    rowsSlide.forEach(row => {
+        tBody.appendChild(row);
+    });
+}
+// start pagination table
+function createPreviousBtn() {
+    let previousBtn = document.createElement("button");
+    previousBtn.classList.add("previous");
+    previousBtn.innerText = "previous";
+
+    if (currentSlide === 1) previousBtn.classList.add("un-active"); else  previousBtn.classList.remove("un-active");
+
+    return previousBtn;
+}
+function createPaginationContainer(currentSlide=1, allSlidesNumber='100', rows=null) {
+    let containerDiv = document.createElement("div");
+    containerDiv.classList.add("pagination-container");
+
+    // Statistics
+    let divStatistics = document.createElement("div");
+    divStatistics.classList.add("statistics");
+    let spanNum = document.createElement("span");
+    spanNum.classList.add("count");
+    spanNum.innerText = currentSlide + ' ';
+    divStatistics.appendChild(spanNum);
+
+    // create from word
+    let spanWord = document.createElement("span");
+    spanWord.innerText = "from";
+    divStatistics.appendChild(spanWord);
+
+    let spanFrom = document.createElement("span");
+    spanFrom.classList.add("from");
+    spanFrom.innerText = ' ' + allSlidesNumber;
+
+    divStatistics.appendChild(spanFrom);
+
+    containerDiv.appendChild(divStatistics);
+
+    // Create Bar
+    let divBar = document.createElement("div");
+    divBar.classList.add("bar");
+
+
+    // previous button
+    let previousBnt = createPreviousBtn(currentSlide)
+    divBar.appendChild(previousBnt);
+
+    // create pagination
+    let divPagination = document.createElement("div");
+    divPagination.classList.add("pagination");
+
+    // Add Number In pagination
+
+    divBar.appendChild(divPagination);
+
+    // Next Button
+    let nextBtn = document.createElement("button");
+    nextBtn.classList.add("next");
+    nextBtn.innerText = "Next";
+    divBar.appendChild(nextBtn);
+
+    containerDiv.appendChild(divBar);
+
+    return containerDiv;
+}
+function getRowsTable(table) {
+    return table.querySelectorAll("tbody tr");
+}
+function displaySlide(rows, tBody, rowsBerSlid, pageNumber) {
+    tBody.innerHTML = '';
+    pageNumber --;
+
+    let start = rowsBerSlid * pageNumber;
+    let end  = start + rowsBerSlid;
+
+    let rowsSlide = Array.from(rows).slice(start, end);
+
+    rowsSlide.forEach(row => {
+        tBody.appendChild(row);
+    });
+}
+function paginationButtons(slideNumber, rows) {
+    let button = document.createElement("button");
+    button.classList.add("num");
+    button.textContent = slideNumber;
+
+    if (currentSlide === slideNumber) {
+        button.classList.add("active");
+        currentBtn = button;
+    }
+
+    if (slideNumber === 1 || slideNumber === '1') {
+        previous.classList.remove("un-visible");
+    } else {
+        previous.classList.add("un-visible");
+    }
+
+
+
+    button.addEventListener("click", () => {
+        currentBtn.classList.remove("active");
+        currentSlide = slideNumber;
+        currentBtn = button;
+        currentBtn.classList.add("active")
+
+        if (currentSlide === 1) {
+            previous.classList.add("un-active");
+        } else {
+            previous.classList.remove("un-active");
+        }
+
+        setupPagination(rows, tBody.parentElement.nextElementSibling.querySelector('.pagination'), shownRowsNumber)
+        displaySlide(rows, tBody, shownRowsNumber, currentSlide);
+        numberSlideContainer.innerText = slideNumber + ' ';
+    });
+
+    return button
+}
+
+function setupPagination(rows, pagination, rowsBerSlid) {
+
+    pagination.innerHTML = '';
+    let slideCount = Math.ceil(rows.length / rowsBerSlid);
+
+    let start = currentSlide - numberPaginationInRow <= 0 ? 1 : currentSlide - numberPaginationInRow ;
+
+    let end = currentSlide + numberPaginationInRow > slideCount ? slideCount : currentSlide + numberPaginationInRow ;
+
+    for (let i = start; i <= end; i++) {
+        let button = paginationButtons(i, rows);
+        pagination.appendChild(button)
+    }
+}
+let rows        = [];
+let tableNumber = 0;
+const shownRowsNumber = 2;
+let currentSlide        = 1;
+let allSlidesNumber = null;
+let tBody = null;
+let currentBtn = null;
+let numberSlideContainer = null;
+let numberPaginationInRow = 5;
+let previous = null;
+let next = null;
+
+const tables = document.querySelectorAll(".pagination-table");
+tables.forEach(table => {
+
+
+    rows[tableNumber] = getRowsTable(table);
+    let trs = rows[tableNumber];
+
+    allSlidesNumber = Math.ceil(rows[tableNumber].length / shownRowsNumber);
+
+    // Show First Slide
+    let container = createPaginationContainer(currentSlide, allSlidesNumber, rows[tableNumber]);
+    insertAfter(table, container);
+
+    displaySlide(rows[tableNumber], table.querySelector("tbody"), shownRowsNumber, currentSlide);
+    tBody = table.querySelector("tbody");
+
+    previous = tBody.parentElement.nextElementSibling.querySelector(".previous");
+    next = tBody.parentElement.nextElementSibling.querySelector(".next");
+
+    previous.addEventListener("click", () => {
+
+        let startPosition = (currentSlide * shownRowsNumber) - (shownRowsNumber * 2);
+
+        let endPosition = startPosition + shownRowsNumber;
+
+        let rows = [...trs];
+        rows = rows.slice(startPosition, endPosition);
+
+
+        tBody.innerHTML ='';
+        rows.forEach(row => {
+            tBody.appendChild(row);
+        });
+
+        currentBtn.classList.remove("active");
+        if (startPosition === 0 && endPosition === shownRowsNumber) {
+            previous.classList.add("un-active");
+            currentBtn = container.querySelector(".pagination").firstElementChild;
+        } else {
+            currentBtn = currentBtn.previousElementSibling;
+            previous.classList.remove("un-active");
+        }
+
+        currentBtn.classList.add("active");
+        currentSlide--;
+
+
+        // Shuffle Buttons
+
+        let pagination = container.querySelector(".pagination");
+        pagination.innerHTML = '';
+        let slideCount = Math.ceil(trs.length / shownRowsNumber);
+
+
+        let start = currentSlide - numberPaginationInRow <= 0 ? 1 : currentSlide - numberPaginationInRow ;
+
+        let end = currentSlide + numberPaginationInRow > slideCount ? slideCount : currentSlide + numberPaginationInRow ;
+
+        for (let i = start; i <= end; i++) {
+            let button = paginationButtons(i, trs);
+            pagination.appendChild(button)
+        }
+        let count = table.nextElementSibling.querySelector(".statistics .count");
+
+        let num  =  count.innerText ;
+        count.innerText = parseInt(num) - 1;
+
+    });
+
+    next.addEventListener("click", () => {
+
+        let startPosition = currentSlide * shownRowsNumber ;
+        let endPosition = startPosition + shownRowsNumber;
+
+        let rows = [...trs];
+
+        rows = rows.slice(startPosition, endPosition);
+
+        tBody.innerHTML ='';
+        rows.forEach(row => {
+            tBody.appendChild(row);
+        });
+
+        currentBtn.classList.remove("active");
+
+        if (endPosition - 1 === trs.length) {
+            next.classList.add("un-active");
+            currentBtn = container.querySelector(".pagination").lastElementChild;
+        } else {
+            currentBtn = currentBtn.nextElementSibling;
+            next.classList.remove("un-active");
+        }
+
+        previous.classList.remove("un-active");
+        currentBtn.classList.add("active");
+        currentSlide++;
+
+
+        // Shuffle Buttons
+
+        let pagination = container.querySelector(".pagination");
+        pagination.innerHTML = '';
+        let slideCount = Math.ceil(trs.length / shownRowsNumber);
+
+
+        let start = currentSlide - numberPaginationInRow <= 0 ? 1 : currentSlide - numberPaginationInRow ;
+
+        let end = currentSlide + numberPaginationInRow > slideCount ? slideCount : currentSlide + numberPaginationInRow ;
+
+        for (let i = start; i <= end; i++) {
+            let button = paginationButtons(i, trs);
+            pagination.appendChild(button)
+        }
+        let count = table.nextElementSibling.querySelector(".statistics .count");
+
+        let num  =  count.innerText ;
+        count.innerText = parseInt(num) + 1;
+
+    });
+
+
+
+    setupPagination(rows[tableNumber], container.querySelector(".pagination"), shownRowsNumber, rows[tableNumber]);
+
+    numberSlideContainer = table.nextElementSibling.querySelector(".statistics .count");
+    numberSlideContainer.innerText = currentSlide + ' ';
+
+
+
+    tableNumber++;
+});
