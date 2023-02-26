a0789bd25da03bb762052c522c85e87c41c5fd19
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-26 12:18:05 +0300

    Update authorizedAccess Method version 1.3 add if url ajax url

--- app/LIB/authentication.php
+++ app/LIB/authentication.php
@@ -39,10 +39,11 @@ class Authentication
      * @param $action $the view you want render
      *
      * @return bool false if user can't access page or controller and true other ways
-     * *@version 1.2
+     * **@version 1.3
      * This method to check if user has authorized access in page
      * if url (controller / action) in $_authorizedURLs array (this array has urls can each user go to it)
      * Or url (controller / action) in privileges user (get it from db)
+     * Or url be ajax url
      *
      * @author Feras Barahmeh
      */
@@ -50,7 +51,8 @@ class Authentication
     {
         $url = '/' . $controller . '/' . $action;
         if (in_array($url, $this->_authorizedURLs)
-            || in_array($url, $this->_session->user->privileges)) {
+            || in_array($url, $this->_session->user->privileges)
+            || str_contains($url, "Ajax")) {
             return true;
         }
         return false;
