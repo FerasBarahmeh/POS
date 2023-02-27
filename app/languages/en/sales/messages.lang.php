1e9153442c09032572620528a6bb8ded7be666bb
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-28 00:16:56 +0300

    add products sale words in languages file

--- app/languages/en/sales/messages.lang.php
+++ app/languages/en/sales/messages.lang.php
@@ -5,3 +5,11 @@ $content["message_client_not_exist"]            = "This Client Not Exist";
 $content["message_email_not_exist"]             = "Not found client hase this email";
 $content["message_hint_search_client"]          = "Search Name Client";
 $content["message_hint_search_client_email"]    = "Search Email Client";
+
+// Product
+$content["message_product_exist"]               = "Find product Success";
+$content["message_product_not_exist"]           = "This product Not Exist";
+$content["message_hint_search_product"]         = "Search Name Product";
+$content["message_hint_search_client_category"] = "Search Category Product";
+$content["message_hint_search_client_category"] = "Search Category Product";
+$content["message_status_message"]              = "Status Product not valid ";
--- app/languages/en/sales/sellproducts.lang.php
+++ app/languages/en/sales/sellproducts.lang.php
@@ -23,3 +23,15 @@ $content["text_Email"]                  = "Email";
 $content["text_PhoneNumber"]            = "Phone number client";
 $content["text_Address"]                = "Address client";
 
+
+$content["text_info_statistic_client"]  = "Show last statistic client";
+
+
+// Products
+$content["text_BuyPrice"]   = "Buy Price";
+$content["text_QuantityChoose"]   = "Quantity";
+$content["text_Quantity"]   = "All Quantity";
+$content["text_BarCode"]    = "Barcode";
+$content["text_Unit"]       = "Unit";
+$content["text_SellPrice"]  = "Sell Price";
+$content["text_Tax"]        = "Tax";
