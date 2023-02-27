b5129d6a1ceba6ecac4153a47ac0c626987bd230
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-02-28 00:17:27 +0300

    create directory to units and status products

---
+++ app/languages/en/units/units.lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$content["unit_Kilo"] = "Kilo";
+$content["unit_Cardboard"] = "Cardboard";
+$content["unit_Box"] = "Box";
+$content["unit_Piece"] = "Piece";
+$content["unit_Meter"] = "Meter";
