c0fec4ac9a9dc4f8f5e72e3cf425f5dad2284f8a
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-03-02 01:20:29 +0300

    create payment type and status class

---
+++ app/enums/PaymentStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace APP\Enums;
+
+class PaymentStatus
+{
+    public int $sent = 0;
+    public int $draft = 1;
+    public int $paid = 1;
+}
---
+++ app/enums/PaymentType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace APP\Enums;
+
+class PaymentType
+{
+    public int $Cash = 0;
+    public int $installments = 1;
+    public int $visaCard = 2;
+}
