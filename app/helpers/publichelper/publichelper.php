b2fb65106d25ffc30e509de6eadf2459a77d5bde
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-04-11 02:07:01 +0300

    create getLastWord and posLastWord and removeLastWord in helper trait

--- app/helpers/publichelper/publichelper.php
+++ app/helpers/publichelper/publichelper.php
@@ -5,7 +5,6 @@ namespace APP\Helpers\PublicHelper;
 
 
 use ReflectionClass;
-use function APP\pr;
 
 trait PublicHelper
 {
@@ -40,5 +39,40 @@ trait PublicHelper
     public function getClassValuesProperties($obj, $getClassName=true): array
     {
         return (new ReflectionClass($obj::class))->getDefaultProperties();
+    }
+
+    public function getLastWord($str): string
+    {
+        $len = strlen($str);
+        $str = trim($str);
+        $lastWord = "";
+
+        for ($i = $len - 1; $i >= 0; $i--) {
+            if (! ctype_space($str[$i])) {
+                $lastWord .= $str[$i];
+            } else {
+                break;
+            }
+        }
+        return $lastWord;
+    }
+    public function posLastWord(string $str): int
+    {
+        $str = rtrim($str);
+
+        $len = strlen($str);
+
+        
+        $i = $len - 1;
+        for (; $i >= 0; $i--) {
+            if (ctype_space($str[$i])) {
+                break;
+            }
+        }
+        return $i;
+    }
+    public function removeLastWord(&$str): void
+    {
+        $str = substr($str, 0, $this->posLastWord($str));
     }
 }
