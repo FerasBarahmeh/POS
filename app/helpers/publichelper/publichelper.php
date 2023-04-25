a93d7cda4908ad5c4681bd6e70b9969ec98ae86f
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-04-25 22:18:16 +0300

    update getSpecificProperties method in publichelper.php and split option

--- app/helpers/publichelper/publichelper.php
+++ app/helpers/publichelper/publichelper.php
@@ -35,31 +35,84 @@ trait PublicHelper
     }
 
     /**
-     * @throws ReflectionException
+     * @param string $str
+     * @return array|false
+     * @version 1.0
+     * To splitting camel case string to words
+     * For example
+     * word = splitCamelCase The Output is [split, Camel, Case]
      * @author Feras Barahmeh
-     * @version 1.2 From getClassValuesProperties() method
-     * To get name and value for each property class
+     */
+    public function splitCamelCase(string $str): array|false
+    {
+        return preg_split(
+            '/(^[^A-Z]+|[A-Z][^A-Z]+)/',
+            $str,
+            -1, /* no limit for replacement count */
+            PREG_SPLIT_NO_EMPTY /*don't return empty elements*/
+            | PREG_SPLIT_DELIM_CAPTURE /*don't strip anything from output array*/
+        );
+    }
+
+    /**
+     * @param array $arr the array you want remove element from
+     * @param array $targetElements all values you wont removed
+     * @return void
      *
+     * If you want to remove a specific element in array O(n)
+     *
+     * @version 1.0
+     * @author Feras Barahmeh
+     */
+    public function unsetSpecificElementArray(array &$arr, array $targetElements): void
+    {
+        foreach ($targetElements as $ele) {
+            unset($arr[$ele]);
+        }
+    }
+
+    /**
      * @param object $obj object from class, you want fetch name properties
      * @param array|string|null $exceptionPropertiesName if you want exception specific property
+     * @param bool $split if you want split name properties to words for example name property is `nameProperties`
+     *       will return name properties as `name Properties`
      *
      * @return ?array associative array the key is a name property the value is a value of property
+     *
+     * @version 1.2
+     *
+     * To get name and value for each property class
+     *
+     * @see getClassValuesProperties() method in this calss
+     *
+     * @throws ReflectionException
+     *
+     * @author Feras Barahmeh
+     *
      */
-    public function getSpecificPropagates(object $obj, array|string|null $exceptionPropertiesName=null): ?array
+    public function getSpecificProperties(object $obj, array|string|null $exceptionPropertiesName=null, bool $split=false): ?array
     {
-        if ($exceptionPropertiesName == null) return (new ReflectionClass($obj::class))->getDefaultProperties();
+        if ($exceptionPropertiesName == null && ! $split)
+            return (new ReflectionClass($obj::class))->getDefaultProperties();
 
-        if (! is_array($exceptionPropertiesName)) $exceptionPropertiesName = [$exceptionPropertiesName];
+        if (! is_array($exceptionPropertiesName))
+            $exceptionPropertiesName = [$exceptionPropertiesName];
 
         $props = (new ReflectionClass($obj::class))->getDefaultProperties();
 
-        foreach ($props as $key => $val) {
-            if (in_array($key, $exceptionPropertiesName)) {
-                unset($props[$key]);
-                return $props;
+        $this->unsetSpecificElementArray($props, $exceptionPropertiesName);
+
+        if ($split) {
+            $new = [];
+            foreach ($props as $key => $val) {
+                $word = $this->splitCamelCase($key);
+                $word = implode(' ', $word);
+                $new[$word] = $val;
             }
+            return $new;
         }
-        return null;
+
+        return $props;
     }
     /**
      * @throws ReflectionException
