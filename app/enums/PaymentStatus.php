d5bddf1d90eed487edd6bcf56087c3e5feccae25
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-04-25 22:19:29 +0300

    update PaymentStatus.php and documentation foreach prop and add PaymentType.php props

--- app/enums/PaymentStatus.php
+++ app/enums/PaymentStatus.php
@@ -4,8 +4,34 @@ namespace APP\Enums;
 
 class PaymentStatus extends AbstractionEnum
 {
-    public int $sent = 0;
-    public int $draft = 1;
-    public int $paid = 2;
+    /**
+     * This is a payment that has begun, but is not complete
+     * @var int
+     */
+    public int $complete = 0;
+    /**
+     * If a Pending payment is never completed it becomes Abandoned after a week.
+     * @var int
+     */
+    public int $abandoned = 1;
+    /**
+     * This is a payment where money has been transferred back to the customer and the customer no longer has access to
+     * the product
+     * @var int
+     */
+    public int $refunded = 2;
+    /**
+     * This is a payment where the payment process failed, whether it be a credit card rejection or some other error.
+     * @var int
+     */
+    public int $failed = 3;
+    /**
+     * Cancelled is used in two different scenarios. One deals with  Recurring Payments. When a subscription is
+     * cancelled then the original payment gets set to cancelled as well.
+     * @var int
+     */
+    public int $cancelled = 4;
+
+
     public static mixed $default = 0;
 }
--- app/enums/PaymentType.php
+++ app/enums/PaymentType.php
@@ -4,8 +4,10 @@ namespace APP\Enums;
 
 class PaymentType extends AbstractionEnum
 {
-    public int $Cash = 0;
-    public int $installments = 1;
-    public int $visaCard = 2;
+    public int $cash = 0;
+    public int $checks = 1;
+    public int $DebitCards = 2;
+    public int $CreditCards = 3;
+    public int $ElectronicBankTransfers = 4;
     public static mixed $default = 0;
 }
