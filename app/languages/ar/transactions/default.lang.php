c4284f1f50a9fd2bd4cd9e95c42eff029c0b2c50
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-06-20 01:23:59 +0300

    add arabic language files to translations page

---
+++ app/languages/ar/transactions/default.lang.php
@@ -0,0 +1,33 @@
+<?php
+$content["title"] = "عملية البيع";
+
+
+$content["title_header"] = "المعاملة";
+
+$content["text_filter_by_transaction_Type"] = "تصفية حسب نوع العملية";
+$content["text_filter_by_date"] = "تصفية حسب التاريخ (من - إلى)";
+$content["text_sales"] = "المبيعات";
+$content["text_purchases"] = "المشتريات";
+$content["text_show_all"] = "عرض الكل";
+$content["text_load"] = "تحميل";
+$content["text_print_report"] = "طباعة التقرير";
+$content["text_print_filtered"] = "طباعة المُصفّى";
+$content["text_id"] = "المعرّف";
+$content["text_type"] = "النوع";
+$content["text_time"] = "الوقت";
+$content["text_num_product"] = "عدد المنتجات";
+$content["text_price"] = "السعر $";
+$content["text_tax"] = "الضريبة";
+$content["text_discount"] = "الخصم";
+$content["text_discount_type"] = "نوع الخصم";
+$content["text_name_transactor"] = "اسم المنفذ";
+$content["text_payment_status"] = "حالة الدفع";
+$content["text_no_disc"] = "بدون خصم";
+$content["text_options"] = "الخيارات";
+
+
+
+
+
+
+
---
+++ app/languages/ar/transactions/messages.lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$content["message_client_exist"]                = "تم ايجاد المشتري بنجاح";
+$content["text_no_disc"] = "لايوجد خصم";
