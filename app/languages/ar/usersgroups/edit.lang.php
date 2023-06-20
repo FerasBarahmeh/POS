88adf9cc62cbc3feab187d36a0a9491e2f9f8126
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-06-20 17:47:57 +0300

    add hint message in arabic file

--- app/languages/ar/usersgroups/edit.lang.php
+++ app/languages/ar/usersgroups/edit.lang.php
@@ -11,4 +11,5 @@ $content["text_name_group"]                     = "اسم المجموعة";
 $content["text_choose_privileges_group"]        = "اختر صلاحيات هذة المجموعة";
 $content["text_choose_save"]                    = "حفظ التغيرات";
 $content["text_message_add_success"]            = "تم تعديل على مجموعة % بنجاح";
+$content["text_message_tip"]                    = "يجب تسجيل الخروج لتفعبل التفعيل التعديلات";
 $content["text_message_edit_field"]             = "عذرا تعذر تعديل المجموعة الرجاء المحاولة لاجقا";
