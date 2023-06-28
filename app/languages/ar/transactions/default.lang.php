4c8fbf653321f251e57bef9134402f2fee357b9c
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-06-28 14:15:15 +0300

    edit transaction language file

--- app/languages/ar/transactions/default.lang.php
+++ app/languages/ar/transactions/default.lang.php
@@ -1,28 +1,30 @@
 <?php
-$content["title"] = "عملية البيع";
+// Title
+$content["title"] = "المعاملات";
 
+// Title Header
+$content["title_header"] = "المعاملات";
 
-$content["title_header"] = "المعاملة";
-
-$content["text_filter_by_transaction_Type"] = "تصفية حسب نوع العملية";
-$content["text_filter_by_date"] = "تصفية حسب التاريخ (من - إلى)";
+$content["text_filter_by_transaction_Type"] = "فلتره المعاملات بنائا على التاريخ";
+$content["text_filter_by_date"] = "تصفية (من الى)";
 $content["text_sales"] = "المبيعات";
 $content["text_purchases"] = "المشتريات";
 $content["text_show_all"] = "عرض الكل";
 $content["text_load"] = "تحميل";
-$content["text_print_report"] = "طباعة التقرير";
-$content["text_print_filtered"] = "طباعة المُصفّى";
-$content["text_id"] = "المعرّف";
+$content["text_print_report"] = "طباعه التقرير";
+$content["text_print_filtered"] = "طباعة التصنيف";
+$content["text_id_invoice"] = "مصنف الفاتورة";
+$content["text_id"] = "الرقم التعريفي";
+$content["text_show"] = "عرض";
+$content["text_download"] = "تحميل";
 $content["text_type"] = "النوع";
 $content["text_time"] = "الوقت";
 $content["text_num_product"] = "عدد المنتجات";
-$content["text_price"] = "السعر $";
+$content["text_price"] = "السعر";
 $content["text_tax"] = "الضريبة";
 $content["text_discount"] = "الخصم";
 $content["text_discount_type"] = "نوع الخصم";
-$content["text_name_transactor"] = "اسم المنفذ";
+$content["text_name_transactor"] = "اسم المعاملة";
 $content["text_payment_status"] = "حالة الدفع";
-$content["text_no_disc"] = "بدون خصم";
+$content["text_no_disc"] = "لايوجد خصم";
 $content["text_options"] = "الخيارات";
-$content["text_show"] = "Show";
-$content["text_download"] = "Download";
---
+++ app/languages/ar/transactions/sales.lang.php
@@ -0,0 +1,6 @@
+<?php
+// Title
+$content["title"] = "معاملات البيع";
+
+// Title Header
+$content["title_header"] = "معاملات البيع";
